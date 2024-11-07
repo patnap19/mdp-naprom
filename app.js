21aaf0740b5a8ded7183d4ddf2815a538f308974
Patryk Napiórkowski <patryk.napiorkowski95@gmail.com>
2024-11-07 10:04:40 +0100

    added logo of MDP and meta tags to <head>

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require("path");
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", (req, res, next) => {
   res.sendFile(path.join(__dirname, "views", "index.html"));
 });
